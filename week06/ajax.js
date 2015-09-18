539d8d4bd53b50ce3afda4065095b47ea3f7c729
Paula Chiu <chiupaula@gmail.com>
2015-09-18 15:03:19 -0700

    add comments to ajax exercise

--- week06/ajax.js
+++ week06/ajax.js
@@ -1,21 +1,31 @@
-$( document ).ready( function() {
-    $('button').click( function(){
-        var xmlh; 
-        xmlh = new XMLHttpRequest();
+$(document ).ready(function() {
+    $('button').click(function(){
+        var xmlh; //initializing the variable called xmlh
 
+        // creating a request object
+        xmlh = new XMLHttpRequest(); //// storing the request object within the variable xmlh
+
+        // xmlh.onreadystatechange allows you to specify a function (callback) to be called whenever the state has changed
         xmlh.onreadystatechange = function() {
+            // 4 or ‘complete’ means the response is complete
             if (xmlh.readyState === 4) {
-                if(xmlh.status === 200) {
+                // xmlh.status check of the status code for the returned response. 
+                if(xmlh.status === 200) { // 200 means OK
+                    // xmlh.responseText means to get the response data as a string
                     document.getElementById("serverTime").innerHTML = xmlh.responseText;
                 } else if (xmlh.status === 400) {
-                    alert('There was an error 400');
+                    alert('There was an error 400.');
                 } else {
-                    alert('something else other than 200 was returned');
+                    alert('Something else other than 200 was returned.');
                 }
             }
         };
         // use URL provided by class
         xmlh.open("GET", "https://hb-server-time.herokuapp.com", true);
+        // xmlh.send sends the request to the specified url
+        // usage: send(string)
+        // string: only used for POST request to pass in parameters
+        // example: xmlh.send(“firstname=Jane&lastname=Doe);
         xmlh.send();        
     });
 });
