b2edde5341c64ae073f006f8094363494095da31
Paula Chiu <chiupaula@gmail.com>
2015-09-18 15:18:05 -0700

    redo previous javascript exercise with jQuery

---
+++ week06/ajax-jQuery.js
@@ -0,0 +1,11 @@
+$(document ).ready(function() {
+    $('button').click(function(){
+        // simplier jQuery equivalent of first ajax exercise
+        $.get("https://hb-server-time.herokuapp.com", function(data) {
+            $("#serverTime").html(data);
+        })
+            .fail(function(){
+                alert("There was an error");
+        });
+    });
+});
